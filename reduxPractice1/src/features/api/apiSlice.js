b805be09612567b7c68ac118558919f2f6f2fb12
Kowsar-Dev <kowsara276@gmail.com>
2025-11-23 11:32:06 +0600

    redux-new change

--- reduxPractice1/src/features/api/apiSlice.js
+++ reduxPractice1/src/features/api/apiSlice.js
@@ -2,18 +2,52 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000/",
+  }),
+
+  tagTypes: ["Users"],
+
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => "users",
+      providesTags: ["Users"],   // GET data will provide tag
+    }),
+
+    addUser: builder.mutation({
+      query: (newUser) => ({
+        url: "users",
+        method: "POST",
+        body: newUser,
+      }),
+      invalidatesTags: ["Users"],  // POST will refresh GET
+    }),
+  }),
+});
+
+export const { useGetUsersQuery, useAddUserMutation } = apiSlice;
+
+/*
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3000/",
   }),
   endpoints: (builder) => ({
     getUsers: builder.query({
       query: () => "users",
+     
     }),
     addUser: builder.mutation({
       query: (newUser) => ({
         url: "users",
         method: "POST",
         body: newUser,
+       
       }),
     }),
   }),
@@ -21,3 +55,6 @@ export const apiSlice = createApi({
 
 // Correct hook names
 export const { useGetUsersQuery, useAddUserMutation } = apiSlice;
+
+
+*/
